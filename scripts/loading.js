2a06d3fbf8117ccef9c7f515743694ffb257f2fb
Arbt7399 <1536864449@qq.com>
2025-04-25 01:35:10 +0800

    888

---
+++ scripts/loading.js
@@ -0,0 +1,52 @@
+const assets = [
+  "./assets/bent.png",
+  "./assets/bg.png",
+  "./assets/bg3.png",
+  "./assets/bg4.png",
+  "./assets/deadH.png",
+  "./assets/deadV.png",
+  "./assets/down_hold.png",
+  "./assets/food1.png",
+  "./assets/food2.png",
+  "./assets/food3.png",
+  "./assets/gameOverPanel1.png",
+  "./assets/gameOverPanel2.png",
+  "./assets/gameOverPanel3.png",
+  "./assets/headH.png",
+  "./assets/headV.png",
+  "./assets/hole.png",
+  "./assets/keyboard_default.png",
+  "./assets/left_hold.png",
+  "./assets/line.png",
+  "./assets/pause_default.png",
+  "./assets/pause_hold.png",
+  "./assets/pause_musicOFF.png",
+  "./assets/pause_musicON.png",
+  "./assets/right_hold.png",
+  "./assets/rushH.png",
+  "./assets/rushV.png",
+  "./assets/sleep.png",
+  "./assets/speed_default.png",
+  "./assets/speed_hold.png",
+  "./assets/straightH.png",
+  "./assets/straightV.png",
+  "./assets/tailH.png",
+  "./assets/tailV.png",
+  "./assets/tip.png",
+  "./assets/up_hold.png"
+]
+
+let loadedCount = 0
+const totalAssets = assets.length
+const progress = document.getElementById('test')
+
+function loadAsset(url) {
+  const asset = new Image()
+  asset.onload = () => {
+    loadedCount++
+    progress.innerHTML = loadedCount
+  }
+  asset.src = url
+}
+
+assets.forEach(loadAsset)
--- scripts/main.js
+++ scripts/main.js
@@ -1152,57 +1152,6 @@ function GameOver() { //游戏结束
   }, s + 350)
 }
 
-window.addEventListener('keydown', function (e) {  //键盘按下
-  e.preventDefault();
-  //空格键暂停
-  if (e.key === ' ') {
-    pauseButtonControl()
-  }
-  else if (pause) return
-
-  switch (e.key) {
-    case 'ArrowUp':
-      dirToUp()
-      break
-    case 'ArrowDown':
-      dirToDown()
-      break
-    case 'ArrowLeft':
-      dirToLeft()
-      break
-    case 'ArrowRight':
-      dirToRight()
-      break
-    case 's':
-      if (!speedUp) speedStart()
-      break
-  }
-
-  //初始状态：按方向键开始游戏
-  //settle结束，方向键继续游戏
-  if (e.key === 'ArrowUp' || e.key === 'ArrowDown' || e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
-    gameOnControl()
-  }
-
-  //死亡状态：按空格键回到初始状态
-  if (gameOver && e.key === ' ' && maxScoreText.style.visibility === 'visible') {
-    gameOver = false
-    gameOverPanelContainer.style.visibility = 'hidden'
-    maxScoreText.style.visibility = 'hidden'
-    currentScoreText.style.visibility = 'hidden'
-    init()
-  }
-})
-
-window.addEventListener('keyup', function (e) {  //键盘松开
-  e.preventDefault();
-  if (e.key === 's') speedEnd()
-
-  if (e.key === 'ArrowUp' || e.key === 'ArrowDown' || e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
-    dirControlButton.style.backgroundImage = 'url(./assets/keyboard_default.png)'
-  }
-})
-
 function musicControl() {  //音量键控制
   if (pausePanel.style.visibility === 'visible') {
     if (musicIsOn) {
@@ -1218,16 +1167,6 @@ function musicControl() {  //音量键控制
   }
 }
 
-musicON.addEventListener('click', function (e) {  //音量键(鼠标)
-  e.preventDefault();
-  musicControl()
-})
-
-musicON.addEventListener('touchstart', function (e) {  //音量键(触屏)
-  e.preventDefault();
-  musicControl()
-})
-
 function continueButtonControl() {  //'继续'按钮控制
   if (pausePanel.style.visibility === 'visible') {
     pause = false
@@ -1237,16 +1176,6 @@ function continueButtonControl() {  //'继续'按钮控制
   }
 }
 
-continueButton.addEventListener('click', function (e) {  //继续(鼠标)
-  e.preventDefault();
-  continueButtonControl()
-})
-
-continueButton.addEventListener('touchstart', function (e) {  //继续(触屏)
-  e.preventDefault();
-  continueButtonControl()
-})
-
 function againControl() {  //'再玩一次'按钮控制
   if (gameOver && gameOverPanelContainer.style.visibility === 'visible') {
     gameOver = false
@@ -1254,16 +1183,6 @@ function againControl() {  //'再玩一次'按钮控制
     init()
   }
 }
-
-again.addEventListener('touchstart', function (e) {  //再玩一次(触屏)
-  e.preventDefault();
-  againControl()
-})
-
-again.addEventListener('click', function (e) {  //再玩一次(鼠标)
-  e.preventDefault();
-  againControl()
-})
 
 function pauseButtonControl() {  //暂停键控制
   if (!gameOver) {
@@ -1283,11 +1202,6 @@ function pauseButtonControl() {  //暂停键控制
   }
 }
 
-pauseButton.addEventListener('touchstart', function (e) {  //暂停键
-  e.preventDefault();
-  pauseButtonControl()
-})
-
 function speedStart() {  //加速开始
   if (!speedUp) {
     speedUp = true
@@ -1305,21 +1219,6 @@ function speedEnd() {  //加速结束
     foodSpeed32 = defaultSpeed
   }
 }
-
-speedButton.addEventListener('touchstart', function (e) {  //加速键按住
-  e.preventDefault();
-  speedStart()
-})
-
-speedButton.addEventListener('touchend', function (e) {  //抬起：加速取消
-  e.preventDefault();
-  speedEnd()
-})
-
-key.addEventListener('touchend', function (e) {  //抬起：方向键取消
-  e.preventDefault();
-  dirControlButton.style.backgroundImage = 'url(./assets/keyboard_default.png)'
-})
 
 function dirToUp() {
   if (!gameOver) {
@@ -1409,61 +1308,3 @@ function gameOnControl() {  //初始状态：按方向键开始游戏 //settle�
     gameLoop()
   }
 }
-
-document.addEventListener('touchmove', function (e) {  //方向键按住拖动  
-  e.preventDefault()
-  if (settling) return
-  const touch = [...e.touches]
-  touch.forEach((obj) => {
-    const x = obj.clientX - (keyboardLeft + (windowWidth - gameWidth) / 2)
-    const y = obj.clientY - (keyboardTop + gameWidth + Top)
-    if (x > -30 / 659 * windowHeight && y > -60 / 659 * windowHeight && x < dirControlWidth + 60 / 659 * windowHeight && y < dirControlWidth + 60 / 659 * windowHeight) {
-      // console.log(parseInt(x) + ', ' + parseInt(y));
-      if (x < y && x + y < dirControlWidth) {
-        dirToLeft()
-        gameOnControl()
-      }
-      else if (x > y && x + y < dirControlWidth) {
-        dirToUp()
-        gameOnControl()
-      }
-      else if (x > y && x + y > dirControlWidth) {
-        dirToRight()
-        gameOnControl()
-      }
-      else if (x < y && x + y > dirControlWidth) {
-        dirToDown()
-        gameOnControl()
-      }
-    }
-  })
-}, { passive: false })
-
-document.addEventListener('touchstart', function (e) {  //方向键点击
-  e.preventDefault();
-  if (settling) return
-  const touch = [...e.touches]
-  touch.forEach((obj) => {
-    const x = obj.clientX - (keyboardLeft + (windowWidth - gameWidth) / 2)
-    const y = obj.clientY - (keyboardTop + gameWidth + Top)
-    if (x > -30 / 659 * windowHeight && y > -60 / 659 * windowHeight && x < dirControlWidth + 60 / 659 * windowHeight && y < dirControlWidth + 60 / 659 * windowHeight) {
-      // console.log(parseInt(x) + ', ' + parseInt(y));
-      if (x < y && x + y < dirControlWidth) {
-        dirToLeft()
-        gameOnControl()
-      }
-      else if (x > y && x + y < dirControlWidth) {
-        dirToUp()
-        gameOnControl()
-      }
-      else if (x > y && x + y > dirControlWidth) {
-        dirToRight()
-        gameOnControl()
-      }
-      else if (x < y && x + y > dirControlWidth) {
-        dirToDown()
-        gameOnControl()
-      }
-    }
-  })
-}, { passive: false })
