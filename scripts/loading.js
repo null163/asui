942ad0041e040ce5f99ccd7a6bd7a0f5c77a327d
Arbt7399 <1536864449@qq.com>
2025-04-25 10:33:40 +0800

    4.25 0

--- scripts/loading.js
+++ scripts/loading.js
@@ -1,47 +1,43 @@
 const images = [
   "./assets/bent.png",
-  "./assets/bg.png",
-  "./assets/bg3.png",
-  "./assets/bg4.png",
-  "./assets/deadH.png",
-  "./assets/deadV.png",
-  "./assets/down_hold.png",
-  "./assets/food1.png",
-  "./assets/food2.png",
-  "./assets/food3.png",
-  "./assets/gameOverPanel1.png",
-  "./assets/gameOverPanel2.png",
-  "./assets/gameOverPanel3.png",
   "./assets/headH.png",
   "./assets/headV.png",
-  "./assets/hole.png",
-  "./assets/keyboard_default.png",
-  "./assets/left_hold.png",
-  "./assets/line.png",
-  "./assets/pause_default.png",
-  "./assets/pause_hold.png",
-  "./assets/pause_musicOFF.png",
-  "./assets/pause_musicON.png",
-  "./assets/right_hold.png",
+  "./assets/deadH.png",
+  "./assets/deadV.png",
   "./assets/rushH.png",
   "./assets/rushV.png",
   "./assets/sleep.png",
-  "./assets/speed_default.png",
-  "./assets/speed_hold.png",
   "./assets/straightH.png",
   "./assets/straightV.png",
   "./assets/tailH.png",
   "./assets/tailV.png",
+  "./assets/food1.png",
+  "./assets/food2.png",
+  "./assets/food3.png",
+  "./assets/hole.png",
+  "./assets/speed_default.png",
+  "./assets/speed_hold.png",
+  "./assets/pause_default.png",
+  "./assets/pause_hold.png",
+  "./assets/gameOverPanel1.png",
+  "./assets/gameOverPanel2.png",
+  "./assets/gameOverPanel3.png",
+  "./assets/pause_musicOFF.png",
+  "./assets/pause_musicON.png",
+  "./assets/keyboard_default.png",
+  "./assets/up_hold.png",
+  "./assets/down_hold.png",
+  "./assets/left_hold.png",
+  "./assets/right_hold.png",
   "./assets/tip.png",
-  "./assets/up_hold.png"
-]
-
-const BGMs = [
-  "./sounds/CuteJump.mp3"
+  "./assets/bg.png",
+  "./assets/line.png",
+  "./assets/bg3.png",
+  "./assets/bg4.png",
 ]
 
 let loadedCount = 0
-const totalAssets = images.length + BGMs.length
+const totalAssets = images.length
 const progress = document.getElementById('test')
 
 function loadImage(url) {
@@ -49,17 +45,10 @@ function loadImage(url) {
   asset.onload = () => {
     loadedCount++
     progress.innerHTML = loadedCount
-    if (loadedCount === totalAssets) addScript()
-  }
-  asset.src = url
-}
-
-function loadBGM(url) {
-  const asset = new Audio()
-  asset.onload = () => {
-    loadedCount++
-    progress.innerHTML = loadedCount
-    if (loadedCount === totalAssets) addScript()
+    if (loadedCount === totalAssets) {
+      progress.innerHTML = 'done'
+      addScript()
+    }
   }
   asset.src = url
 }
@@ -71,4 +60,11 @@ function addScript() {
 }
 
 images.forEach(loadImage)
-BGMs.forEach(loadBGM)
+
+document.addEventListener('touchstart', function (e) {
+  e.preventDefault();
+}, { passive: false })
+
+document.addEventListener('touchmove', function (e) {
+  e.preventDefault();
+}, { passive: false })
